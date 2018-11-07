15297a1950784602d882843bd720ab44e7e5b139
Evan Procter <evanprocter@gmail.com>
2018-11-07 09:15:44 -0500

    added updates to users

--- index.js
+++ index.js
@@ -8,87 +8,145 @@ User.searchByName('aylin')
         console.log(users);
     });
 
-User.getById(6)
-    .then(allUsers => {
-        u.delete();
-    });
+// User.getById(6)
+//     .then(allUsers => {
+//         u.delete();
+//     });
 
-User.deleteById(8);
+// User.deleteById(8);
 
-User.getAll()
-    .then(allUsers => {
-        allUsers.forEach(user => {
-            console.log(user.name);
-        })
-    });
+// User.getAll()
+//     .then(allUsers => {
+//         allUsers.forEach(user => {
+//             console.log(user.name);
+//         })
+//     });
 
-User.getById(1)
-    .then(userFromDB => {
-        console.log(userFromDB);
-        userFromDB.getTodos()
-            .then(todos => {
-                console.log(todos);
-            })
-    });
+// User.getById(1)
+//     .then(userFromDB => {
+//         console.log(userFromDB);
+//         userFromDB.getTodos()
+//             .then(todos => {
+//                 console.log(todos);
+//             })
+//     });
 
-const beth = new User(2, 'beth');
-beth.getTodos()
-    .then(result => { console.log(result); })    
+// const beth = new User(2, 'beth');
+// beth.getTodos()
+//     .then(result => { console.log(result); })    
 
-let newUsers = [
-    'jeff',
-    'brandy',
-    'zack',
-    'tasha',
-    'jenn',
-    'cori'
-];
+// let newUsers = [
+//     'jeff',
+//     'brandy',
+//     'zack',
+//     'tasha',
+//     'jenn',
+//     'cori'
+// ];
 
-newUsers.forEach(u => {
-    User.add(u)
-        .then(aNewUser => {
-            aNewUser.addTodo('do the thing');
-        })
-})
+// newUsers.forEach(u => {
+//     User.add(u)
+//         .then(aNewUser => {
+//             aNewUser.addTodo('do the thing');
+//         })
+// })
 
-const skyler = new User('Skyler the Dog');
-const ahjuma = new User('Ahjuma the Impressive');
+// const skyler = new User('Skyler the Dog');
+// const ahjuma = new User('Ahjuma the Impressive');
 
-// debugger;
+// // debugger;
 
-skyler.greet(ahjuma);
-ahjuma.greet(skyler);
+// skyler.greet(ahjuma);
+// ahjuma.greet(skyler);
 
-let u = User.findById(1);
-u.name = 'eileeeeeeen';
-u.save();
+// let u = User.findById(1);
+// u.name = 'eileeeeeeen';
+// u.save();
 
-// deleting the user by their name
-User.deleteById('alsdfj;alsdjflasj')
-    .then(result => {console.log(result); })
+// // deleting the user by their name
+// User.deleteById('alsdfj;alsdjflasj')
+//     .then(result => {console.log(result); })
 
-// deleting the user by id
-Todo.deleteById(1)
-    .then(result => { console.log(result); }
+// // deleting the user by id
+// Todo.deleteById(1)
+//     .then(result => { console.log(result); }
     
-// getting the todos for the user
-User.getTodosForUser(3)
-    .then(result => {console.log(result); })
+// // getting the todos for the user
+// User.getTodosForUser(3)
+//     .then(result => {console.log(result); })
 
-Todo.assignToUser(2, 2)
-    .then(() => {
-        User.getTodosForUser(2)
-        .then(result => { console.log(result); })
-    })
+// Todo.assignToUser(2, 2)
+//     .then(() => {
+//         User.getTodosForUser(2)
+//         .then(result => { console.log(result); })
+//     })
 
-Todo.assignToUser(3, 2)
-    .then(() => {
-        User.getTodosForUser(2)
-        .then(result => { console.log(result); })
-    })
+// Todo.assignToUser(3, 2)
+//     .then(() => {
+//         User.getTodosForUser(2)
+//         .then(result => { console.log(result); })
+//     })
 
-Todo.assignToUser(4, 5)
-    .then(() => {
-        User.getTodosForUser(2)
-        .then(result => { console.log(result); })
-    })
+// Todo.assignToUser(4, 5)
+//     .then(() => {
+//         User.getTodosForUser(2)
+//         .then(result => { console.log(result); })
+//     })
+
+// Todo.assignToUser(1, 5)
+//     .then(() => {
+//         User.getTodosForUser(2)
+//         .then(result => { console.log(result); })
+//     })
+
+// User.getAll()
+//     .then(result => {console.log(result); })
+
+
+
+
+// User.getAll()
+//     .then(results => {
+//         console.log(results);
+//         console.log(`yep those were the users. cool.`)
+//     })
+
+// User.getById('chris')
+//     .then(result => { console.log(result); })
+
+// Todo.getById(200000)
+//     .then(result => { console.log(result); })
+
+// User.add('jeff')
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// Todo.add('walk the chewbacca', false)
+//     .catch(err => {
+//         console.log(err);
+//     })
+//     .then(result => {
+//         console.log(result);
+//     })
+
+
+
+
+// User.updateName(6, 'JEEEEEEEEEEEf')
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// Todo.markCompleted(1)
+//     .then(result => {
+//         console.log(result);
+//     })
+
+
+
+
+// User.deleteById(6)
+//     .then(result => {
+//         console.log(result.rowCount);
+//     })
